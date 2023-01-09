0416aac202ca69f5d74dbb3dd4ceb2c0f7e8ea11
fld46 <fredimp@orange.fr>
2023-01-10 00:48:21 +0100

    test

--- src/components/Logements.js
+++ src/components/Logements.js
@@ -1,12 +1,12 @@
 import logementListe from '../datas/logements.json';
-console.log(logementListe);
-function Logements() {
+console.log(logementListe.find(({ id }) => id === "b9123946"));
+export function Logements() {
     return (
         <ul>
             {logementListe.map((logement) => (<li key={logement.id}>{logement.title}</li>))}
-
         </ul>
     );
 }
+
 
 export default Logements;
