508f71b16ceedbc1a2f9ab40a13e209c93fdc31d
fld46 <fredimp@orange.fr>
2023-02-03 09:15:53 +0100

    css

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -1,39 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Collapse = ({ title, content }) => {
-    const isCollapse = (e) => {
-        e.preventDefault();
-        const divText = e.target.nextSibling;
-        const arrow = e.target.lastChild;
-
-        if (!divText.classList.contains("show")) {
-            divText.classList.add("show");
-            arrow.classList.add("rotate");
-        } else {
-            divText.classList.remove("show");
-            arrow.classList.remove("rotate");
-        }
+    const [open, setOpen] = useState(false);
+    const isCollapse = () => {
+        setOpen(!open);
     };
+
+    let classAnim = "";
+    if (open) {
+        classAnim += " rotate";
+    } else {
+        classAnim = "collapse__arrow";
+    }
+
     return (
-        <div className="collapse ">
+        <div className="collapse">
             <button type="button" className="collapse__button" onClick={isCollapse}>
                 {title}
-
-                <p className="collapse__arrow">&lt;</p>
+                <p className={classAnim}>&lt;</p>
             </button>
-            <div className="collapse__content">
-                {Array.isArray(content) ? (
-                    <ul className="collapse__list">
-                        {content.map((equipment, index) => (
-                            <li key={index} className="collapse__list-element">
-                                {equipment}
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p className="collapse__text">{content}</p>
-                )}
-            </div>
+            {
+                open && (
+                    <div >
+                        {Array.isArray(content) ? (
+                            <ul className="collapse__list">
+                                {content.map((equipment, index) => (
+                                    <li key={index} className="collapse__list-element">
+                                        {equipment}
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p className="collapse__text">{content}</p>
+                        )}
+                    </div>
+                )
+            }
         </div>
     );
 };
