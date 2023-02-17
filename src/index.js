1eb571b499eca48e9bca3bc8e4404de16ec81814
fld46 <fredimp@orange.fr>
2023-02-17 08:01:19 +0100

    css ok

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import Footer from './components/Footer';
 import Error from './components/Error';
 import './styles/index.scss';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -26,5 +24,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-
